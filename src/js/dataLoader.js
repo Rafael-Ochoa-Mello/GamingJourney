96d77ce3eada1e69aa50c453dfd03b81a2a3ce44
rafaelochoamello <rafaelochoamello@gmail.com>
2023-12-10 22:13:56 -0300

    📦 ...

---
+++ src/js/dataLoader.js
@@ -0,0 +1,8 @@
+const gameData = require('../gameData/data.json');
+
+function dataLoader(){
+    // const title = document.getElementById('gameTitle');
+    // title.innerHTML = gameData[0].title;
+    console.log("!");
+}
+
